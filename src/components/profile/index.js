2f0a2ec532cd6f3d6824a5e23eb4338c6deb517f
Kartanovicius <donatas.karta@gmail.com>
2021-06-15 09:52:34 +0300

    Profile h-0 delete

--- src/components/profile/index.js
+++ src/components/profile/index.js
@@ -38,7 +38,7 @@ export default function Profile({user}) {
 
     return (
         <div className="md:grid grid-cols-3 gap-4">
-            <div className="grid col-span-1 gap-4 h-0">
+            <div className="grid col-span-1 gap-4">
                 <Header
                     postsCount={postsCollection ? postsCollection.length : 0}
                     profile={profile}
